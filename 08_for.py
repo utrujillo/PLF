c1e0ad48140370c317e708f0b043c219210b5dad
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-10-14 07:32:56 -0500

    Update ejercicio 08

--- 08_for.py
+++ 08_for.py
@@ -1,6 +1,10 @@
 for contador in range(1,11):
   print("Contador: %i" %( contador ))
 
+vocales = "aeuio"
+
 cadena = "Habitantes"
-for letra in cadena:
-  print("Letra: %s" %(letra) )
+for index,letra in enumerate(cadena):
+  if index % 2 == 0:
+    if( letra in vocales ):
+      print("Posicion %i letra par es %s" %( index, letra ))
