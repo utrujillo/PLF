d9e9e45eb931c546f3d7a72309b1c59b21f3bfdd
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-11-28 02:18:23 -0600

    Actualizando usuario 	modified:   clases/usuario.py 	modified:   inicio.py

--- inventario/clases/usuario.py
+++ inventario/clases/usuario.py
@@ -31,19 +31,7 @@ Listado de usuarios
 	
 	def crear(self):
 		os.system('cls')
-		self.usuario["nombre"] = input("Nombre: ")
-		self.usuario["apellidos"] = input("Apellidos: ")
-
-		for tipo in TipoUsuario:
-			print( tipo.describe() )
-		tipo_usuario = int( input( "Seleccionar el tipo de usuario: " ) )
-		
-		if( tipo_usuario > len(TipoUsuario) ):
-			print( 'Usuario no existente, se asignara un valor nulo' )
-			self.usuario["tipo_usuario"] = ""
-		else:
-			self.usuario["tipo_usuario"] = TipoUsuario(tipo_usuario)
-		
+		self.usuario = self.solicitar_usuario()
 		self.usuario["id"] = len(self.usuarios) + 1
 		
 		self.usuarios = self.usuarios.append( self.usuario, ignore_index = True )
@@ -66,4 +54,42 @@ Listado de usuarios encontrados
 		id_buscar = int( input("ID a buscar: ") )
 		coincidencia = self.usuarios.query('id == {0}'.format(id_buscar))
 		return coincidencia
+	
+	def actualizar(self):
+		os.system('cls')
+		# Verificando que el usuario exista
+		usuario = self.buscar_id()
+		if( len(usuario) > 0 ):
+			print( 'Usuario encontrado, favor de ingresar los nuevos datos' )
+			print( usuario )
+			# Obteniendo el registro del listado de usuarios
+			usuario_original = self.usuarios.loc[ usuario.index ]
+			# Solicitando la nueva informacion para el usuario
+			self.usuario = self.solicitar_usuario()
+			# Uniendo los datos del usuario original, con la nueva informacion (exeptuando el ID y el campo visible)
+			usuario_original["nombre"] = self.usuario["nombre"]
+			usuario_original["apellidos"] = self.usuario["apellidos"]
+			usuario_original["tipo_usuario"] = self.usuario["tipo_usuario"]
+			# Actualizando dentro del catalogo de usuarios el nuevo usuario
+			self.usuarios.loc[ usuario.index ] = usuario_original
+			self.usuarios.to_excel( self.FILE )
+			print( "La informacion fue actualizada con exito" )
+		else:
+			print("El usuario no fue encontrado...")
+	
+	def solicitar_usuario(self):
+		self.usuario["nombre"] = input("Nombre: ")
+		self.usuario["apellidos"] = input("Apellidos: ")
+
+		for tipo in TipoUsuario:
+			print( tipo.describe() )
+		tipo_usuario = int( input( "Seleccionar el tipo de usuario: " ) )
+		
+		if( tipo_usuario > len(TipoUsuario) ):
+			print( 'Usuario no existente, se asignara un valor nulo' )
+			self.usuario["tipo_usuario"] = ""
+		else:
+			self.usuario["tipo_usuario"] = TipoUsuario(tipo_usuario)
+		
+		return self.usuario
 
--- inventario/inicio.py
+++ inventario/inicio.py
@@ -2,30 +2,37 @@ from menu.menu import menu, seleccion_submenu
 from clases.usuario import Usuario
 
 def run():
-    salir = False
-    usuario = Usuario()
-    
-    while salir == False:
-        menu()
-        opcion = int(input('Selecciona una opcion: '))
+	salir = False
+	usuario = Usuario()
 
-        seleccion_submenu( opcion )
-        opcion_submenu = int(input('Que operacion deseas realizar: '))
-        
-        if( opcion_submenu  == 1 ):
-            usuario.listar_todo()
-        elif ( opcion_submenu == 2 ):
-            usuario.crear()
-        elif ( opcion_submenu == 3 ):
-            usuario.buscar_texto()
-        elif ( opcion_submenu == 4 ):
-            coincidencia = usuario.buscar_id()
-            print( coincidencia )
-        
-        terminar = input('Deseas realizar otra operacion? S/N ')
-        
-        if( terminar == "N" or terminar == "n" ):
-            salir = True
+	while salir == False:
+		menu()
+		opcion = int(input('Selecciona una opcion: '))
+
+		seleccion_submenu( opcion )
+		opcion_submenu = int(input('Que operacion deseas realizar: '))
+
+		if( opcion_submenu  == 1 ):
+			usuario.listar_todo()
+		elif ( opcion_submenu == 2 ):
+			usuario.crear()
+		elif ( opcion_submenu == 3 ):
+			usuario.buscar_texto()
+		elif ( opcion_submenu == 4 ):
+			coincidencia = usuario.buscar_id()
+			mensaje = '''
+Identificador encontrado
+**************************
+		'''
+			print(mensaje)
+			print( coincidencia )
+		elif( opcion_submenu == 5 ):
+			usuario.actualizar()
+
+		terminar = input('Deseas realizar otra operacion? S/N ')
+
+		if( terminar == "N" or terminar == "n" ):
+			salir = True
 
 if __name__ == '__main__':
-    run()
+	run()
